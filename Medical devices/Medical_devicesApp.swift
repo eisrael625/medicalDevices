b04e6694bf956b15f8d157456568f0fb7aa260fd
eisrael625 <117659847+eisrael625@users.noreply.github.com>
2026-04-01 00:23:08 -0400

    Initial Commit

---
+++ Medical devices/Medical_devicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medical_devicesApp.swift
+//  Medical devices
+//
+//  Created by Eytan Israel on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Medical_devicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
